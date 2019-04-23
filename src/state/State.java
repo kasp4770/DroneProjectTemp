8f2faf283c327eb227036eab09c3f56fba1b7e26
kasp4770 <kasp4770@edu.easj.dk>
2019-04-23 13:04:11 +0200

    Changed to default again since it affected Logger and Observerclasses.

--- src/state/State.java
+++ src/state/State.java
@@ -1,10 +1,11 @@
 package state;
 
-public class State {
+public abstract class State {
 
     public void push(Button b) {
         b.setCurrent(Inactive.instance());
         System.out.println("    Drone is inactive");
     }
+    public abstract String getCurrentState();
 
 }
